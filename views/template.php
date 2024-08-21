60ff05a9136162e1eedc52c3077a625ea0ca2ec5
Hugo Sauvage <124774769+hugosk8@users.noreply.github.com>
2024-08-21 17:09:45 +0200

    ajout des fonts

--- views/template.php
+++ views/template.php
@@ -29,6 +29,10 @@
     <link rel="icon" type="image/png" sizes="32x32" href="assets/images/studio46/favicon.ico" />
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/studio46/favicon.ico" />
     <link rel="manifest" href="assets/images/favicons/site.webmanifest" />
+    <!-- Baskerville -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
 
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
